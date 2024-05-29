32a66ec01f9b4054a02c91e0c3dfd27ec586cd92
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 16:32:24 +0530

    Added: `discord-moderation-bot/src/commands/roleManagement.py`

---
+++ discord-moderation-bot/src/commands/roleManagement.py
@@ -0,0 +1,24 @@
+class RoleManagement:
+    def __init__(self):
+        pass
+
+    def assign_role(self, user_id, role_name):
+        pass
+
+    def remove_role(self, user_id, role_name):
+        pass
+
+    def modify_role(self, user_id, old_role_name, new_role_name):
+        pass
+
+    def check_role(self, user_id, role_name):
+        pass
+
+    def get_user_roles(self, user_id):
+        pass
+
+    def get_role_members(self, role_name):
+        pass
+
+    def get_all_roles(self):
+        pass
